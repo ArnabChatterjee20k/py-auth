47a4e804537de833ea703c2424a96aee14ffd84c
arnab <arnabchatterjee.ac.2@gmail.com>
2025-09-19 23:04:02 +0530

    refined model to have extension to serializability

--- src/pyauth/models/model.py
+++ src/pyauth/models/model.py
@@ -1,11 +1,27 @@
 from abc import ABC
-from dataclasses import dataclass, asdict, fields
+from dataclasses import dataclass, asdict, fields, MISSING, field
 from typing import ClassVar
+from typing import get_origin, get_args, Union, Optional
+from types import UnionType
+import datetime
+
+
+class MissingDefault:
+    pass
+
+
+class CurrentTimeStamp:
+    pass
 
 
 @dataclass
 class Model(ABC):
+    # making both exclude and id as class var to ignore during data representation(fields() ignore classvar)
     exclude: ClassVar[list[str]] = []
+    id: ClassVar[Optional[int]] = field(
+        default=None,
+        metadata={"primary_key": True, "index": True, "auto_increment": True},
+    )
 
     def to_dict(self) -> dict:
         data = asdict(self)
@@ -13,3 +29,80 @@ class Model(ABC):
 
     def get_fields(self):
         return {f.name for f in fields(self)}
+
+    # todo: Have a mechanism to ignore some fields externally by passing ignorable values in the function
+    def get_values(self):
+        """
+        Return a dictionary representing the values to be inserted in the DB.
+        If user provided the value then use it else for the default dont use it
+        Or if the schema allows using None
+        """
+        insert_data = {}
+        for f in fields(self):
+            origin = get_origin(f.type)
+            if f.name in self.exclude:
+                continue
+
+            value = getattr(self, f.name, None)
+            if value is None:
+                # check if schema allows None or not
+                if origin is Union or origin is UnionType:
+                    types = [
+                        t.__name__ if hasattr(t, "__name__") else str(t)
+                        for t in get_args(f.type)
+                    ]
+                    if "NoneType" in types:
+                        insert_data[f.name] = value
+            else:
+                insert_data[f.name] = value
+
+        return insert_data
+
+    @classmethod
+    def get_schema(cls):
+        """Generate json schema; default values are ignored and only default_factory are considered"""
+        schema = {}
+        for field in fields(cls):
+            field_type = field.type
+            field_name = field.name
+            origin = get_origin(field_type)
+            metadata = field.metadata
+            default = MissingDefault()
+            if field.default_factory is not MISSING:
+                if isinstance(field_type, type) and issubclass(
+                    field_type, datetime.datetime
+                ):
+                    default = CurrentTimeStamp()
+
+                else:
+                    default = field.default_factory()
+
+            schema[field_name] = {
+                "type": None,
+                "default": default,
+                "primary_key": metadata.get("primary_key", False),
+                "index": metadata.get("index", False),
+                "unique": metadata.get("unique", False),
+                "auto_increment": metadata.get("auto_increment", False),
+            }
+            if origin is Union or origin is UnionType:
+                schema[field_name]["type"] = [
+                    t.__name__ if hasattr(t, "__name__") else str(t)
+                    for t in get_args(field_type)
+                ]
+            elif (
+                origin in (list, dict)
+                or str(field_type).startswith("typing.List")
+                or str(field_type).startswith("typing.Dict")
+            ):
+                schema[field_name]["type"] = "json"
+            else:
+                schema[field_name]["type"] = [
+                    (
+                        field_type.__name__
+                        if hasattr(field_type, "__name__")
+                        else str(field_type)
+                    )
+                ]
+
+        return schema
